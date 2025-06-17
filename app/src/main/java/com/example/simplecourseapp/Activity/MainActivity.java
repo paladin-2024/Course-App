46cabba2e2162d8453adf4dd2901bbc22b603d9c
paladin-2024 <cnzabb@gmail.com>
2025-06-18 02:58:04 +0300

    Refactor: Move Activities to 'Activity' package
    This commit moves `MainActivity` and `IntroActivity` into a new `Activity` sub-package.
    
    The following files were updated to reflect this change:
    - `app/src/main/AndroidManifest.xml`
    - `app/src/main/res/layout/activity_intro.xml`
    - `app/src/main/res/layout/activity_main.xml`
    
    R class imports were also added to `MainActivity.java` and `IntroActivity.java`.

--- app/src/main/java/com/example/simplecourseapp/Activity/MainActivity.java
+++ app/src/main/java/com/example/simplecourseapp/Activity/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.simplecourseapp;
+package com.example.simplecourseapp.Activity;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.example.simplecourseapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
