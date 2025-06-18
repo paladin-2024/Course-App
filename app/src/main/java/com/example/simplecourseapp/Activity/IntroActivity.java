73f7a44bf425015efdfc5c914f3bbb9880f312ef
paladin-2024 <cnzabb@gmail.com>
2025-06-18 16:21:54 +0300

    Rename start button and add click listener
    The `ImageView` with the ID `imageView5` in `activity_intro.xml` has been renamed to `startbtn`.
    
    In `IntroActivity.java`, a click listener has been added to the `startbtn` ImageView. When clicked, it will start `MainActivity`. The `ViewCompat.setOnApplyWindowInsetsListener` has been removed.

--- app/src/main/java/com/example/simplecourseapp/Activity/IntroActivity.java
+++ app/src/main/java/com/example/simplecourseapp/Activity/IntroActivity.java
@@ -1,6 +1,9 @@
 package com.example.simplecourseapp.Activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,10 +20,14 @@ public class IntroActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_intro);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        ImageView startBtn=findViewById(R.id.startbtn);
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(IntroActivity.this,MainActivity.class));
+            }
         });
+
     }
 }
