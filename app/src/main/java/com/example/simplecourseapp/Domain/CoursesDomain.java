93fe5782f4a5a88a1b609c4a3c9156d0c29997e4
paladin-2024 <cnzabb@gmail.com>
2025-06-18 03:02:02 +0300

    Add CoursesDomain class
    A new Java class, `CoursesDomain.java`, has been added to the `com.example.simplecourseapp.Domain` package.
    
    This class represents a course and includes the following private fields:
    - `title` (String)
    - `price` (double)
    - `picPath` (String)
    
    It includes a constructor to initialize these fields and public getter and setter methods for each field.

---
+++ app/src/main/java/com/example/simplecourseapp/Domain/CoursesDomain.java
@@ -0,0 +1,37 @@
+package com.example.simplecourseapp.Domain;
+
+public class CoursesDomain {
+    private String title;
+    private double price;
+    private String picPath;
+
+    public CoursesDomain(String title, String picPath, double price) {
+        this.title = title;
+        this.price = price;
+        this.picPath = picPath;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getPicPath() {
+        return picPath;
+    }
+
+    public void setPicPath(String picPath) {
+        this.picPath = picPath;
+    }
+}
